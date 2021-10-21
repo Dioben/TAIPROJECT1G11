8a9a415ac87816d0d5dfacc00e0b756f216dfabb
buckaroo69 <diogobento@ua.pt>
2021-10-21 13:48:17 +0100

    get word frequencies done

--- bin/common_modules.py
+++ bin/common_modules.py
@@ -4,8 +4,17 @@ def getFileFrequencies(filename,order):
     text = open(filename,"r").read()
     alphabet = set(text)
                 #if (len(alphabet)**(order*2))*64>8...   #assumes int size is 64, actually grows forever -> actually just always using hash table
-    start_text = text[:order]
+    current_buffer = text[:order]
     text = text[order:]
     table = {}
+
     for character in text:
-        pass
+        if current_buffer not in table.keys(): #have we seen this predecessor yet?
+            table[current_buffer]= {}
+        if character not in table[current_buffer].keys(): #have we seen this sequence?
+            table[current_buffer][character]=1
+        else:
+            table[current_buffer][character]+=1
+        current_buffer = current_buffer [1:]+character
+    
+    return table,alphabet
