27cc85d71f34190cddceec3b165b62601fb6fa82
buckaroo69 <diogobento@ua.pt>
2021-10-27 10:19:16 +0100

    fixes sh iteration

--- bin/datagen.sh
+++ bin/datagen.sh
@@ -6,7 +6,7 @@ for smoothing in {0.0001,0.001,0.01}
 do
 echo "${order} ${smoothing} $(python3 fcm.py --source ../example/example.txt --order $order --smoothing $smoothing)" >>result.txt
 done
-for smoothing in {0.1..0.9..0.1}
+for smoothing in {0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9} #iterating with float step is nonsensical, thank you bash
 do
 echo "${order} ${smoothing} $(python3 fcm.py --source ../example/example.txt --order $order --smoothing $smoothing)" >>result.txt
 done
